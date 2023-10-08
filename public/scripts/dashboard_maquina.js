e0c94c9e95ad6e076aee82ad1010415e911caecd
YuriOlivs <yuri.silva@sptech.com>
2023-10-07 21:14:28 -0300

    adicionando graficos do google charts a dashboard de maquinas

---
+++ public/scripts/dashboard_maquina.js
@@ -0,0 +1,132 @@
+google.charts.load('current', {packages: ['corechart']});
+
+function drawCPU() {
+   var data = new google.visualization.DataTable();
+   data.addColumn("string", "Data/Hora");
+   data.addColumn("number", "% de uso");
+   data.addRows([
+      ['12:30', 70],
+      ['12:31', 71],
+      ['12:32', 75],
+      ['12:33', 75],
+      ['12:34', 80],
+      ['12:35', 88],
+      ['12:36', 89],
+      ['12:37', 90],
+      ['12:38', 91],
+      ['12:39', 88],
+      ['12:40', 96],
+      ['12:41', 80],
+   ]);
+
+   var options = {
+      title: null,
+      legend: 'none',
+      chartArea: {'width': '100%'},
+      backgroundColor: '#f5f5f5',
+      colors: ['#9747FF']
+   };
+
+   var chart = new google.visualization.AreaChart(document.getElementById("cpu_chart"));
+   chart.draw(data, options);
+}
+
+function drawRAM() {
+   var data = new google.visualization.DataTable();
+   data.addColumn("string", "Data/Hora");
+   data.addColumn("number", "% de uso");
+   data.addRows([
+      ['12:30', 80],
+      ['12:31', 81],
+      ['12:32', 85],
+      ['12:33', 85],
+      ['12:34', 80],
+      ['12:35', 78],
+      ['12:36', 59],
+      ['12:37', 80],
+      ['12:38', 91],
+      ['12:39', 99],
+      ['12:40', 70],
+      ['12:41', 80],
+   ]);
+
+   var options = {
+      title: null,
+      legend: 'none',
+      chartArea: {'width': '100%'},
+      backgroundColor: '#f5f5f5',
+      colors: ['#9747FF']
+   };
+
+   var chart = new google.visualization.AreaChart(document.getElementById("ram_chart"));
+   chart.draw(data, options);
+}
+
+function drawDisc() {
+   var data = new google.visualization.DataTable();
+   data.addColumn("string", "Data/Hora");
+   data.addColumn("number", "% de uso");
+   data.addRows([
+      ['12:30', 80],
+      ['12:31', 81],
+      ['12:32', 85],
+      ['12:33', 85],
+      ['12:34', 80],
+      ['12:35', 78],
+      ['12:36', 59],
+      ['12:37', 80],
+      ['12:38', 91],
+      ['12:39', 99],
+      ['12:40', 70],
+      ['12:41', 80],
+   ]);
+
+   var options = {
+      title: null,
+      legend: 'none',
+      chartArea: {'width': '100%'},
+      backgroundColor: '#f5f5f5',
+      colors: ['#9747FF']
+   };
+
+   var chart = new google.visualization.AreaChart(document.getElementById("disc_chart"));
+   chart.draw(data, options);
+}
+
+function drawWindow() {
+   var data = new google.visualization.DataTable();
+   data.addColumn("string", "Data/Hora");
+   data.addColumn("number", "% de uso");
+   data.addRows([
+      ['12:30', 80],
+      ['12:31', 81],
+      ['12:32', 85],
+      ['12:33', 85],
+      ['12:34', 80],
+      ['12:35', 78],
+      ['12:36', 59],
+      ['12:37', 80],
+      ['12:38', 91],
+      ['12:39', 99],
+      ['12:40', 70],
+      ['12:41', 80],
+   ]);
+
+   var options = {
+      title: null,
+      legend: 'none',
+      chartArea: {'width': '100%'},
+      backgroundColor: '#f5f5f5',
+      colors: ['#9747FF']
+   };
+
+   var chart = new google.visualization.AreaChart(document.getElementById("window_chart"));
+   chart.draw(data, options);
+}
+
+google.charts.setOnLoadCallback(() => {
+   drawCPU();
+   drawRAM();
+   drawDisc();
+   drawWindow();
+});
