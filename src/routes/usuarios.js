b4b0c513ae7ebf6810188e0b012af8b782d7dbb4
GabrielYukioMC <gabriel.coca@sptech.school>
2023-10-09 20:59:28 -0300

    CRUD Usuario v1.0.3

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,15 +1,23 @@
 var express = require("express");
 var router = express.Router();
 
-var usuarioController = require("../controllers/usuarioController");
+var usuarioController = require("../controllers/avisoController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+//Recebendo os dados do html e direcionando para a função cadastrar de avisoController.js
 router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
+    usuarioController.publicar(req, res);
 })
 
-router.get("/entrar/:emailVar/:senhaVar", function (req, res) {
-    usuarioController.entrar(req, res);
+router.put("/editar", function (req, res) {
+    usuarioController.editar(req, res);
+});
+
+router.get("/mostrar_dados", function (req, res) {
+    usuarioController.mostrar_dados(req, res);
+});
+
+router.delete("/deletar", function (req, res) {
+    usuarioController.deletar(req, res);
 });
 
 module.exports = router;
