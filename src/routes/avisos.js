41f8364f9a3014349bd3fa5fde93c14d6099c0a0
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-09 21:03:59 -0300

    Criando rota pros avisos

---
+++ src/routes/avisos.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var router = express.Router();
+
+var avisoController = require("../controllers/avisoController");
+
+router.get("/", function (req, res) {
+    avisoController.testar(req, res);
+});
+
+router.get("/listar/:idUsuario", function (req, res) {
+    avisoController.listar(req, res);
+});
+
+
+router.get("/mostrar_dados/:idAviso", function (req, res) {
+    avisoController.mostrar_dados(req, res);
+});
+
+
+router.get("/pesquisar/:descricao", function (req, res) {
+    avisoController.pesquisarDescricao(req, res);
+});
+
+router.post("/publicar/:idUsuario", function (req, res) {
+    avisoController.publicar(req, res);
+});
+
+router.put("/editar/:idAviso", function (req, res) {
+    avisoController.editar(req, res);
+});
+
+router.delete("/deletar/:idAviso", function (req, res) {
+    avisoController.deletar(req, res);
+});
+
+module.exports = router;
