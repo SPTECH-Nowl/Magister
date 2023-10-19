81e628a8a4356269a1aef4f03fe5c262c73adf5e
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-18 21:59:17 -0300

    100% olhos

--- public/scripts/login_cadastro.js
+++ public/scripts/login_cadastro.js
@@ -128,13 +128,13 @@ var status = 1;
 function senha(){
 	if(status > 0){
       document.getElementById('senha_input').type='text';
-      document.getElementById("olho_login").classList.remove("icon_hide")
-      document.getElementById("olho_login").classList.add("icon")
+      document.getElementById("olho_login").classList.remove("icon")
+      document.getElementById("olho_login").classList.add("icon_hide")
       status = 0;
     }else{
     document.getElementById('senha_input').type='password';
-    document.getElementById("olho_login").classList.remove("icon")
-      document.getElementById("olho_login").classList.add("icon_hide")
+    document.getElementById("olho_login").classList.remove("icon_hide")
+      document.getElementById("olho_login").classList.add("icon")
       status = 1;
     
     }
@@ -147,13 +147,13 @@ function senha_cadastro(){
 
 	if(status > 0){
       document.getElementById('senha_input_cadastro').type='text';
-      document.getElementById("olho").classList.remove("icon_senha_desativado")
-      document.getElementById("olho").classList.add("icon_senha")
+      document.getElementById("olho").classList.remove("icon_senha")
+      document.getElementById("olho").classList.add("icon_senha_desativado")
       status = 0;
     }else{
     document.getElementById('senha_input_cadastro').type='password';
-    document.getElementById("olho").classList.remove("icon_senha")
-    document.getElementById("olho").classList.add("icon_senha_desativado")
+    document.getElementById("olho").classList.remove("icon_senha_desativado")
+    document.getElementById("olho").classList.add("icon_senha")
       status = 1;
     
     }
@@ -165,13 +165,13 @@ function confirmar_senha_cadastro(){
 
 	if(status > 0){
       document.getElementById('conf_senha_input').type='text';
-      document.getElementById("olho_confirmar").classList.remove("icon_confirmar_desativado")
-      document.getElementById("olho_confirmar").classList.add("icon_confirmar")
+      document.getElementById("olho_confirmar").classList.remove("icon_confirmar")
+      document.getElementById("olho_confirmar").classList.add("icon_confirmar_desativado")
       status = 0;
     }else{
     document.getElementById('conf_senha_input').type='password';
-    document.getElementById("olho_confirmar").classList.remove("icon_confirmar")
-    document.getElementById("olho_confirmar").classList.add("icon_confirmar_desativado")
+    document.getElementById("olho_confirmar").classList.remove("icon_confirmar_desativado")
+    document.getElementById("olho_confirmar").classList.add("icon_confirmar")
       status = 1;
     
     }
