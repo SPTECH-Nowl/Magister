9bb58c8ccdc85f6502eb83a40ecb3475c258694e
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-18 21:48:23 -0300

    função olhos

--- public/scripts/login_cadastro.js
+++ public/scripts/login_cadastro.js
@@ -127,12 +127,12 @@ var status = 1;
 
 function senha(){
 	if(status > 0){
-      document.getElementById('senha_input').type='password';
+      document.getElementById('senha_input').type='text';
       document.getElementById("olho_login").classList.remove("icon_hide")
       document.getElementById("olho_login").classList.add("icon")
       status = 0;
     }else{
-    document.getElementById('senha_input').type='text';
+    document.getElementById('senha_input').type='password';
     document.getElementById("olho_login").classList.remove("icon")
       document.getElementById("olho_login").classList.add("icon_hide")
       status = 1;
@@ -146,12 +146,12 @@ var status_senha = 1;
 function senha_cadastro(){
 
 	if(status > 0){
-      document.getElementById('senha_input_cadastro').type='password';
+      document.getElementById('senha_input_cadastro').type='text';
       document.getElementById("olho").classList.remove("icon_senha_desativado")
       document.getElementById("olho").classList.add("icon_senha")
       status = 0;
     }else{
-    document.getElementById('senha_input_cadastro').type='text';
+    document.getElementById('senha_input_cadastro').type='password';
     document.getElementById("olho").classList.remove("icon_senha")
     document.getElementById("olho").classList.add("icon_senha_desativado")
       status = 1;
@@ -164,12 +164,12 @@ var status_confirmar_senha = 1;
 function confirmar_senha_cadastro(){
 
 	if(status > 0){
-      document.getElementById('conf_senha_input').type='password';
+      document.getElementById('conf_senha_input').type='text';
       document.getElementById("olho_confirmar").classList.remove("icon_confirmar_desativado")
       document.getElementById("olho_confirmar").classList.add("icon_confirmar")
       status = 0;
     }else{
-    document.getElementById('conf_senha_input').type='text';
+    document.getElementById('conf_senha_input').type='password';
     document.getElementById("olho_confirmar").classList.remove("icon_confirmar")
     document.getElementById("olho_confirmar").classList.add("icon_confirmar_desativado")
       status = 1;
