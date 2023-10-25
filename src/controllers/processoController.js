00353f28c55039bb2e8420aed4ae26e2d254a942
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-25 15:02:41 -0300

    back end quase completo 70% feito

---
+++ src/controllers/processoController.js
@@ -0,0 +1,287 @@
+var  processoModel = require("../models/processoModel");
+
+var sessoes = [];
+
+function listar(req, res) {
+    var codInstituicao = req.params.codInstituicao;
+
+   processoModel.listar(codInstituicao).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!");
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarAdm(req, res) {
+    var codInstituicao = req.params.codInstituicao;
+
+   processoModel.listarAdm(codInstituicao).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!");
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function pesquisarProcesso(req, res) {
+    var instituicao = req.params.instituicao;
+    var nomeProcesso = req.params.nomeProcesso
+
+    processoModel.pesquisarProcesso(nomeProcesso, instituicao).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!");
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarInstrutor(req, res) {
+    var codInstituicao = req.params.codInstituicao;
+
+        processoModel.listarInstrutor(codInstituicao).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!");
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarPorProcesso(req, res) {
+    var idProcesso = req.params.idProcesso;
+
+    processoModel.listarPorProcesso(idProcesso)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "Houve um erro ao buscar os avisos: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+
+
+function mostrar_dadosProcesso(req, res) {
+    var idProcesso = req.params.idProcesso;
+
+   processoModel.mostrar_dadosProcesso(idProcesso)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao deletar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function cadastrarNaDashProcesso(req, res) {
+    var nomeProgramaVar = req.body.nomePrograma;
+    var nomeProcessoVar = req.body.nomeProcesso;
+    var instituicaoVar = req.body.instituicao;
+
+    console.log(nomeProgramaVar, nomeProcessoVar)
+    console.log("tamo no controller")
+    
+    if (nomeProgramaVar == undefined) {
+        res.status(400).send("Seu nome está indefinido");
+    } else if (nomeProcessoVar == undefined) {
+        res.status(400).send("Seu email está indefinido");
+    } else if (instituicaoVar == undefined) {
+        res.status(400).send("Seu código hexadecimal está indefinido");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivoprocessoModel.js
+       processoModel.cadastrarNaDashProcesso(nomeProgramaVar,nomeProcessoVar, instituicaoVar)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+function editarProcesso(req, res) {
+    var nomeProgramaVar = req.body.nomePrograma;
+    var nomeProcessoVar = req.body.nomeProcesso;
+    var idProcesso = req.body.idProcesso;
+
+   processoModel.editarProcesso(nomePrograma, nomeProcesso, idProcesso)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function deletarProcesso(req, res) {
+    var idProcesso = req.body.idProcessoPE;
+
+    processoModel.deletarProcesso(idProcesso)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao deletar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function testar(req, res) {
+    console.log("ENTRAMOS NA processoController");
+    res.json("ESTAMOS FUNCIONANDO!");
+}
+
+
+function qtdTotal(req, res){
+    var instituicao = req.params.instituicao
+    
+    if (instituicao == undefined) {
+        res.status(400).send("A instituicao não foi capturada!")
+    } else {
+       processoModel.qtdTotal(instituicao)            
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+    }
+
+}
+
+function qtdAdministrador(req, res){
+    var instituicao = req.params.instituicao
+    
+    if (instituicao == undefined) {
+        res.status(400).send("A instituicao não foi capturada!")
+    } else {
+        processoModel.qtdAdministrador(instituicao)            
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+    }
+
+}
+
+function qtdInstrutor(req, res){
+    var instituicao = req.params.instituicao
+    
+    if (instituicao == undefined) {
+        res.status(400).send("A instituicao não foi capturada!")
+    } else {
+        processoModelModel.qtdInstrutor(instituicao)            
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+    }
+
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    listarAdm,
+    listarInstrutor,
+
+    listarPorProcesso,
+    cadastrarNaDashProcesso,
+    editarProcesso,
+    deletarProcesso,
+    mostrar_dadosProcesso,
+
+    qtdTotal,
+    qtdAdministrador,
+    qtdInstrutor,
+
+   
+}
---
+++ src/routes/processo.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var router = express.Router();
+
+var processoController = require("../controllers/processoController");
+
+
+router.get("/listar/:codInstituicao", function (req, res) {
+   processoController.listar(req, res);
+});
+
+router.get("/listarAdm/:codInstituicao", function (req, res) {
+   processoController.listarAdm(req, res);
+});
+
+router.get("/listarInstrutor/:codInstituicao", function (req, res) {
+   processoController.listarInstrutor(req, res);
+});
+
+router.get("/listarPorProcesso/:idProcesso", function (req, res) {
+   processoController.listarPorUsuario(req, res);
+});
+
+router.get("/pesquisarProcesso/:nomeProcesso/:instituicao", function (req, res) {
+   processoController.pesquisarUsuario(req, res);
+});
+
+
+router.get("/mostrar_dadosProcesso/:idProcesso", function (req, res) {
+   processoController.mostrar_dados(req, res);
+});
+
+router.post("/cadastrarNaDashProcesso", function (req, res) {
+   processoController.cadastrarNaDashProcesso(req, res);
+})
+
+router.get("/pesquisar/:descricao", function (req, res) {
+   processoController.pesquisarDescricao(req, res);
+});
+
+
+
+
+router.post("/publicar/:idProcesso", function (req, res) {
+   processoController.publicar(req, res);
+});
+
+router.put("/editarProcesso", function (req, res) {
+   processoController.editar(req, res);
+});
+
+router.delete("/deletarProcesso/", function (req, res) {
+   processoController.deletar(req, res);
+});
+
+router.put("/editarProcesso", function (req, res) {
+   processoController.editar(req, res);
+});
+
+router.get("/mostrar_dadosProcesso", function (req, res) {
+   processoController.mostrar_dados(req, res);
+});
+
+router.delete("/deletarProcesso", function (req, res) {
+   processoController.deletar(req, res);
+});
+
+
+router.get("/qtdTotal/:instituicao", function (req, res) {
+   processoController.qtdTotal(req, res)
+})
+
+
+router.get("/qtdAdministrador/:instituicao", function (req, res) {
+   processoController.qtdAdministrador(req, res)
+})
+
+router.get("/qtdInstrutor/:instituicao", function (req, res) {
+   processoController.qtdInstrutor(req, res)
+})
+module.exports = router;
