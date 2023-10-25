82aa99898dc5281bdaa12afde8ad2770e3b05479
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-25 15:28:46 -0300

    movi o back end do html usuario.html para usuario.js

--- src/database/config.js
+++ src/database/config.js
@@ -21,8 +21,8 @@ var sql = require('mssql');
 var mySqlConfig = {
     host: "localhost",
     database: "magister",
-    user: "root",
-    password: "Kangs1994.",
+    user: "aluno",
+    password: "aluno",
 };
 
 function executar(instrucao) {
